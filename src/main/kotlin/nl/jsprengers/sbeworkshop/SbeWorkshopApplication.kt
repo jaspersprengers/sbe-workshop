2cb6e2fa1cf1b307582c6b2d2ad41e6ff4af145c
Jasper Sprengers <jasper.sprengers@rabobank.nl>
2023-09-20 16:19:18 +0200

    add missing files

---
+++ src/main/kotlin/nl/jsprengers/sbeworkshop/SbeWorkshopApplication.kt
@@ -0,0 +1,11 @@
+package nl.jsprengers.sbeworkshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SbeWorkshopApplication
+
+fun main(args: Array<String>) {
+	runApplication<SbeWorkshopApplication>(*args)
+}
