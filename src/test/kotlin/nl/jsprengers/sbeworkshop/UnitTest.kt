f8c487caea79cd745493d68e3ac4d823948ad07c
Jasper Sprengers <jasper.sprengers@rabobank.nl>
2023-09-24 07:49:00 +0200

    fix test

---
+++ src/test/kotlin/nl/jsprengers/sbeworkshop/UnitTest.kt
@@ -0,0 +1,13 @@
+package nl.jsprengers.sbeworkshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(classes = [SbeWorkshopApplication::class])
+class UnitTest {
+
+    @Test
+    fun doTest(){
+
+    }
+}
